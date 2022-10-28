f4123f681a869cff4b40d2d3bc8f7dbfc252c508
Valerio Lomanto <ian.maayrkas@gmail.com>
2022-10-28 18:40:40 +0200

    prepared ghpages folder

---
+++ docs/_app/immutable/chunks/2-09faa403.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-1f64c2a4.js";const e=!0;export{t as component,e as server};
--- svelte.config.js
+++ svelte.config.js
@@ -9,8 +9,8 @@ const config = {
 
   kit: {
     adapter: adapter({
-      pages: 'ghpages',
-      assets: 'ghpages',
+      pages: 'docs',
+      assets: 'docs',
     }),
   },
 };
