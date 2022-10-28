4dc4df25029a752ed1f1da73d2bb8f6089f377b1
Valerio Lomanto <ian.maayrkas@gmail.com>
2022-10-28 19:23:12 +0200

    dummy changes to force pages rebuild

--- docs/_app/immutable/chunks/1-68b7dd9c.js
+++ docs/_app/immutable/chunks/1-68b7dd9c.js
@@ -1,1 +1,2 @@
 import{default as t}from"../components/error.svelte-2c59e334.js";export{t as component};
+
--- docs/_app/immutable/chunks/2-b00fee66.js
+++ docs/_app/immutable/chunks/2-b00fee66.js
@@ -1,1 +1,2 @@
 import{default as t}from"../components/pages/_page.svelte-5016578a.js";const e=!0;export{t as component,e as server};
+
