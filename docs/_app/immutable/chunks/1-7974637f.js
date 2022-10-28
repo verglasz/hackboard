6ef4263bdcf030fa162537931574b6d8fd8ee27e
Valerio Lomanto <ian.maayrkas@gmail.com>
2022-10-28 19:13:43 +0200

    rebuilt pages site

--- docs/_app/immutable/chunks/1-7974637f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-6b75f740.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-b00fee66.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-5016578a.js";const e=!0;export{t as component,e as server};
